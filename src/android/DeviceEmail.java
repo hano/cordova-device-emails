5053b5a1a89493cdf68d7961e9db7d2ce148c3bc
Sebastien Roch <roch.sebastien@gmail.com>
2014-07-20 22:47:14 +0200

    initial commit, basic test

---
+++ src/android/DeviceEmail.java
@@ -0,0 +1,56 @@
+
+package net.works_for_me.it.deviceemail;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DeviceEmail extends CordovaPlugin {
+    public static final String TAG = "DeviceEmail";
+
+    /**
+     * Constructor.
+     */
+    public DeviceEmail() {
+    }
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getDeviceEmails")) {
+            JSONObject r = new JSONObject();
+            r.put("email", "someone@example.com");
+            callbackContext.success(r);
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+
+    //--------------------------------------------------------------------------
+    // LOCAL METHODS
+    //--------------------------------------------------------------------------
+
+}
